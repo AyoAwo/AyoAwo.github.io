deffcc5f49b44bab40ac3716f7ea81e39c9f9410
Ayo Awoyemi <ayoawoyemi17@gmail.com>
2023-04-28 19:16:17 +0100

    Page reload on submit fixed

--- Vault/script.js
+++ Vault/script.js
@@ -16,10 +16,10 @@ function passwordMatch(form, x) {
     if(password === passwords[x-1]){
         document.getElementById("letter1Link").hidden = false;
         localStorage.setItem("letter1", "unlocked");
-        return;
+        return true;
     }
     document.getElementById("incorrect1").hidden = false;
-    return;
+    return false;
 
 }
 
